9aa8439c55a5205311bc1e64340fd571ff8a238c
BuildTools <calin.chertes@gmail.com>
2021-01-02 17:07:28 +0200

    Heroku auto commit

---
+++ src/main/java/tk/patsite/Patserverdiscordbot/Command/Commands/EmbedCommand.java
@@ -0,0 +1,66 @@
+package tk.patsite.Patserverdiscordbot.Command.Commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import tk.patsite.Patserverdiscordbot.Command.Command;
+import tk.patsite.Patserverdiscordbot.Settings;
+
+public class EmbedCommand extends Command {
+    @Override
+    public void perform(Message message, String[] args) {
+        if (!message.getAuthor().getId().equals(Settings.PATRICK_ID)) {
+            return;
+        }
+        if (args.length >= 1) {
+            switch (args[0]) {
+                case "rules" -> {
+                    message.getChannel().sendMessage(new EmbedBuilder()
+                            .setDescription("As seen here, there are lots of rules to follow." + Settings.NEWLINE +
+                                    "Be sure to follow all of the rules listed here!" + Settings.NEWLINE +
+                                    "There are this much rules to keep our community safe." + Settings.NEWLINE +
+                                    ":warning: The staff team decides on what action will be acted on you.")
+                            .setColor(0x14baba).setTitle("Patserver Discord Rules").setFooter("Last updated: 8h 22d 12m 20y UTC").build()).queue();
+                    message.getChannel().sendMessage(new EmbedBuilder()
+                            .setDescription("> Do not mention the staff for no reason! " + Settings.NEWLINE +
+                                    "> Do not ask for roles / ranks! " + Settings.NEWLINE +
+                                    "> Do not @mention spam! " + Settings.NEWLINE +
+                                    "> Do not send NSFW content! " + Settings.NEWLINE +
+                                    "> Do not send illegal / pirated content! " + Settings.NEWLINE +
+                                    "> Do not publish personal info! " + Settings.NEWLINE +
+                                    "> No hate speech, offensive language, sexism, racism, homophobia! " + Settings.NEWLINE +
+                                    "> No political / religious discussions! " + Settings.NEWLINE +
+                                    "> No [flaming](https://en.wikipedia.org/wiki/Flaming_(Internet))! " + Settings.NEWLINE +
+                                    "> No trolling. " + Settings.NEWLINE +
+                                    "> No spamming. " + Settings.NEWLINE +
+                                    "> No personal attacks. " + Settings.NEWLINE +
+                                    "> No doxxing people. " + Settings.NEWLINE +
+                                    "> Use spaces and do not send each word on each line! " + Settings.NEWLINE +
+                                    "> No walls of text. " + Settings.NEWLINE +
+                                    "> Do not use excessive caps lock. " + Settings.NEWLINE +
+                                    "> Do not talk other languages other than english." + Settings.NEWLINE)
+                            .setColor(0x14baba).setTitle("Primary chat rules: ").build()).queue();
+                    message.getChannel().sendMessage(new EmbedBuilder()
+                            .setDescription("> No loud noises! " + Settings.NEWLINE +
+                                    "> No high-pitch noises! " + Settings.NEWLINE +
+                                    "> Do not annoy people! " + Settings.NEWLINE +
+                                    "> Chat rules apply here too!")
+                            .setColor(0x14baba).setTitle("Voice chat rules: ").build()).queue();
+                    message.getChannel().sendMessage(new EmbedBuilder()
+                            .setDescription("> No loud noises! " + Settings.NEWLINE +
+                                    "> No high-pitch noises! " + Settings.NEWLINE +
+                                    "> Do not annoy people! " + Settings.NEWLINE +
+                                    "> Chat rules apply here too!")
+                            .setColor(0x14baba).setTitle("Voice chat rules: ").build()).queue();
+                }
+                case "commands" -> {
+
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
--- src/main/java/tk/patsite/Patserverdiscordbot/Settings.java
+++ src/main/java/tk/patsite/Patserverdiscordbot/Settings.java
@@ -7,5 +7,7 @@ public final class Settings {
     public static final String RULE_CHANNEL = "<#768729254200082432>";
     public static final String BOTS_CHANNEL = "<#787953253803491378>";
     public static final String BOTS_CHANNEL_ID = "787953253803491378";
-    public static final int PING_TIMEOUT = 200; // The longer, the more accurate the status command will be, but the users will have to wait longer
+    public static final String NEWLINE = System.getProperty("line.separator");
+    // public static final int PING_TIMEOUT = 200; // The longer, the more accurate the status command will be, but the users will have to wait longer
+    public static final String PATRICK_ID = "300559445988081664";
 }
