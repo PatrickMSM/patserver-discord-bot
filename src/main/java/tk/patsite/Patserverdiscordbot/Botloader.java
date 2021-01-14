7e27c04a2f69e3669e616224350a0d76370dfe66
Me <calin.chertes@gmail.com>
2021-01-14 13:53:57 +0200

    fixed purg

--- src/main/java/tk/patsite/Patserverdiscordbot/Botloader.java
+++ src/main/java/tk/patsite/Patserverdiscordbot/Botloader.java
@@ -3,6 +3,7 @@ package tk.patsite.Patserverdiscordbot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import tk.patsite.Patserverdiscordbot.Command.CommandManager;
 import tk.patsite.Patserverdiscordbot.Events.CommandEvent;
 import tk.patsite.Patserverdiscordbot.Events.MemberJoinEvent;
@@ -26,6 +27,9 @@ public final class Botloader {
                 // Intent so I CAN TRACK MEMBERS
                 .enableIntents(GatewayIntent.GUILD_MEMBERS)
                 .enableIntents(GatewayIntent.GUILD_MESSAGE_REACTIONS)
+
+                // Load all members at startup
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
 
                 // .addEventListener(new EventListener() extends ListenerAdapter or implements EventListener) to add an event listener
                 .addEventListeners(new CommandEvent(commandManager))
