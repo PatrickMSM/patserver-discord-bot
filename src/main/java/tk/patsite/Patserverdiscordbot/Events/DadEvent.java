5d88c84b825f309b2b6376fe2d8e65404b90c3c3
PatrickMSM <calin.chertes@gmail.com>
2021-01-17 16:53:18 +0200

    test dad

--- src/main/java/tk/patsite/Patserverdiscordbot/Events/DadEvent.java
+++ src/main/java/tk/patsite/Patserverdiscordbot/Events/DadEvent.java
@@ -3,7 +3,6 @@ package tk.patsite.Patserverdiscordbot.Events;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DadEvent extends ListenerAdapter {
@@ -18,11 +17,13 @@ public class DadEvent extends ListenerAdapter {
 
         String message = event.getMessage().getContentRaw().trim();
 
+        event.getMessage().reply(message).queue();
+        /*
         final Matcher match = DadPattern.matcher(message);
         if (!match.matches())
             return;
         message = match.group(0).replace(DadPatternIm, "");
 
-        event.getMessage().reply("Hey " + message + ", im dad!").queue();
+        event.getMessage().reply("Hey " + message + ", im dad!").queue(); */
     }
 }
