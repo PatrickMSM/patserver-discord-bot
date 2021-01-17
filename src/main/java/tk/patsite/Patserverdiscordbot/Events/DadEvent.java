129e425c91294958fa6917e314e773c90ffa243f
PatrickMSM <calin.chertes@gmail.com>
2021-01-17 20:09:19 +0200

    last fix dad

--- src/main/java/tk/patsite/Patserverdiscordbot/Events/DadEvent.java
+++ src/main/java/tk/patsite/Patserverdiscordbot/Events/DadEvent.java
@@ -3,32 +3,27 @@ package tk.patsite.Patserverdiscordbot.Events;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DadEvent extends ListenerAdapter {
 
-    private String getT(String str, String conds, String condDelimiter) {
-        final String[] checks = conds.split(condDelimiter);
 
-        for (String check : checks) {
-            String test = str.substring(str.lastIndexOf(check) + 1);
-            if (!test.equals(str)) {
-                return test;
-            }
-        }
-        return null;
-    }
+    Pattern dadtern = Pattern.compile("^(.*)(im|i'm|i am|am|iam) (.*)$", Pattern.CASE_INSENSITIVE);
 
     @Override
     public void onMessageReceived(final MessageReceivedEvent event) {
         if (event.getAuthor().isBot())
             return;
 
-        final String message, result;
-        message = event.getMessage().getContentRaw().trim();
-        result = getT(message, "im|i'm|i am", "|");
 
-        if (result == null)
+        final String message = event.getMessage().getContentRaw();
+        if (!dadtern.matcher(message).matches())
             return;
 
-        event.getMessage().reply("Hey " + result + ", im dad!").queue();
+        final Matcher matcher = dadtern.matcher(message);
+
+
+        event.getMessage().reply("Hey " + matcher.group(3) + ", im dad!").queue();
     }
 }
