17f1321d222344eaffc486442668d33ed7adf27e
PatrickMSM <calin.chertes@gmail.com>
2021-01-17 15:52:22 +0200

    ahh remvoed lock from trq thread again

--- src/main/java/tk/patsite/Patserverdiscordbot/Command/Commands/SpacexCommand.java
+++ src/main/java/tk/patsite/Patserverdiscordbot/Command/Commands/SpacexCommand.java
@@ -1,6 +1,16 @@
 package tk.patsite.Patserverdiscordbot.Command.Commands;
 
+import net.dv8tion.jda.api.entities.Message;
 import tk.patsite.Patserverdiscordbot.Command.Command;
 
 public class SpacexCommand extends Command {
+    @Override
+    public String getDescription() {
+        return "Shows current spacex launch data";
+    }
+
+    @Override
+    public void perform(Message message, String[] args) {
+        //
+    }
 }
